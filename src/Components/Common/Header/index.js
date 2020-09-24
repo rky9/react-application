1ce20915f3187bbc3c65a9a9c08e44499f949b75
rky9 <rajesh.coer@gmail.com>
2020-09-24 05:48:44 +0530

    header info

---
+++ src/Components/Common/Header/index.js
@@ -0,0 +1,13 @@
+import Header from './Header'
+
+import React, { Component } from 'react'
+
+export default class index extends Component {
+    render() {
+        return (
+            <div>
+                <Header/>
+            </div>
+        )
+    }
+}
